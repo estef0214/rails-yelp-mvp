e812ae020322c4c6567bf9979cc5d88cb910b83a
Estefani Perez <estefani.perez271@gmail.com>
2022-05-25 12:03:34 +0100

    made Restaurants and Reviews migrations, made seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new({
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: Restaurant::CATEGORY.sample,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  }
+  )
+  restaurant.save!
+end
